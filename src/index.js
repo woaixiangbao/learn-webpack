79c58e068829d1bd496f66675b594ab6041780b9
jianqing <jianqing@ptminddeMacBook-Pro-2.local>
2017-12-22 16:17:48 +0800

    搭建一个基本的文件框架

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './style.css';
+import img from './iphone-x-pic.jpg';
+import printMe from './print.js';
+function component(){
+	var element = document.createElement('div');
+	element.innerHTML = _.join(['hello', 'webpack'], ' ');
+	element.classList.add('hello');
+	var myImg = new Image();
+	myImg.src = img;
+	element.appendChild(myImg);
+	var btn = document.createElement('button');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	element.appendChild(btn);
+	return element;
+}
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+    entry: {
+        app:'./src/index.js',
+        print:'./src/print.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Output Management'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use:[
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
